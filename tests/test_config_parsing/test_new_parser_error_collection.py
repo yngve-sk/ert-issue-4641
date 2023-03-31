f39c13d3b7d7eec57f06c4d3646b4d589d827278
Yngve S. Kristiansen <yngve.s.kristiansen@webstep.no>
2023-03-31 12:44:33 +0200

    Place common test functionality in one function

--- tests/test_config_parsing/test_new_parser_error_collection.py
+++ tests/test_config_parsing/test_new_parser_error_collection.py
@@ -9,165 +9,172 @@ test_config_file_base = "test"
 test_config_file_name = f"{test_config_file_base}.ert"
 
 
+def assert_error_from_config_with(
+    contents: str,
+    expected_line: int,
+    expected_column: int,
+    expected_end_column: int,
+    file_name: str = "test.ert",
+    other_files: dict = None,
+):
+    with open(file_name, "w", encoding="utf-8") as fh:
+        fh.write(contents)
+
+    if other_files is not None:
+        for other_file_name, content in other_files.items():
+            with open(other_file_name, mode="w", encoding="utf-8") as fh:
+                fh.writelines(content)
+
+    collected_errors = []
+    ErtConfig.from_file(
+        file_name, use_new_parser=True, collected_errors=collected_errors
+    )
+
+    for error in collected_errors:
+        assert error.filename == file_name
+
+    error_locations = [(x.line, x.column, x.end_column) for x in collected_errors]
+    expected_error_loc = (expected_line, expected_column, expected_end_column)
+
+    assert expected_error_loc in error_locations, (
+        f"Expected error location {expected_error_loc} to be found in list of"
+        f"error locations: {error_locations}"
+    )
+
+
 @pytest.mark.usefixtures("use_tmpdir")
 def test_info_queue_content_negative_value_invalid():
-    test_config_contents = dedent(
-        """
+    assert_error_from_config_with(
+        contents="""
 NUM_REALIZATIONS  1
 DEFINE <STORAGE> storage/<CONFIG_FILE_BASE>-<DATE>
 RUNPATH <STORAGE>/runpath/realization-<IENS>/iter-<ITER>
 ENSPATH <STORAGE>/ensemble
 QUEUE_SYSTEM LOCAL
 QUEUE_OPTION LOCAL MAX_RUNNING -4
-"""
-    )
-    with open(test_config_file_name, "w", encoding="utf-8") as fh:
-        fh.write(test_config_contents)
-
-    collected_errors = []
-    ErtConfig.from_file(
-        test_config_file_name,
-        use_new_parser=True,
-        collected_errors=collected_errors,
+        """,
+        expected_line=7,
+        expected_column=32,
+        expected_end_column=34,
     )
 
-    error: ErrorInfo = collected_errors[0]
-    assert error.filename == test_config_file_name
-    assert error.line == 7
-    assert error.column == 32
-    assert error.end_column == 34
 
-
+@pytest.mark.usefixtures("use_tmpdir")
 def test_info_summary_given_without_eclbase_gives_error(tmp_path):
-    (tmp_path / "config.ert").write_text(
-        """NUM_REALIZATIONS 1
-SUMMARY summary"""
+    assert_error_from_config_with(
+        contents="""
+NUM_REALIZATIONS 1
+SUMMARY summary""",
+        expected_line=3,
+        expected_column=1,
+        expected_end_column=8,
     )
-
-    collected_errors = []
-    ErtConfig.from_file(
-        str(tmp_path / "config.ert"),
-        collected_errors=collected_errors,
-        use_new_parser=True,
-    )
-
-    error: ErrorInfo = collected_errors[0]
-
-    assert error.line == 2
-    assert error.column == 1
-    assert error.end_column == 8
 
 
 @pytest.mark.usefixtures("use_tmpdir")
 def test_info_gen_kw_with_incorrect_format(tmp_path):
-    with open(test_config_file_name, "w", encoding="utf-8") as fh:
-        fh.write(
-            """
+    assert_error_from_config_with(
+        contents="""
 JOBNAME my_name%d
 NUM_REALIZATIONS 1
-GEN_KW KW_NAME template.txt kw.txt prior.txt INIT_FILES:custom_param0
-"""
-        )
-
-    with open("template.txt", mode="w", encoding="utf-8") as fh:
-        fh.writelines("MY_KEYWORD <MY_KEYWORD>")
-
-    with open("prior.txt", mode="w", encoding="utf-8") as fh:
-        fh.writelines("MY_KEYWORD NORMAL 0 1")
-
-    collected_errors = []
-    ErtConfig.from_file(
-        test_config_file_name,
-        use_new_parser=True,
-        collected_errors=collected_errors,
+GEN_KW KW_NAME template.txt kw.txt priors.txt INIT_FILES:custom_param0
+""",
+        expected_line=4,
+        expected_column=47,
+        expected_end_column=71,
+        other_files={
+            "template.txt": "MY_KEYWORD <MY_KEYWORD>",
+            "priors.txt": "MY_KEYWORD NORMAL 0 1",
+        },
     )
-
-    error: ErrorInfo = collected_errors[0]
-    assert error.filename == test_config_file_name
-    assert error.line == 4
-    assert error.column == 46
-    assert error.end_column == 70
 
 
 @pytest.mark.usefixtures("use_tmpdir")
 def test_info_gen_kw_forward_init(tmp_path):
-    with open(test_config_file_name, "w", encoding="utf-8") as fh:
-        fh.write(
-            """
+    assert_error_from_config_with(
+        contents="""
 JOBNAME my_name%d
 NUM_REALIZATIONS 1
 GEN_KW KW_NAME template.txt kw.txt priors.txt FORWARD_INIT:True INIT_FILES:custom_param
-"""
-        )
-
-    with open("template.txt", mode="w", encoding="utf-8") as fh:
-        fh.writelines("MY_KEYWORD <MY_KEYWORD>")
-
-    with open("priors.txt", mode="w", encoding="utf-8") as fh:
-        fh.writelines("MY_KEYWORD NORMAL 0 1")
-
-    collected_errors = []
-    ErtConfig.from_file(
-        test_config_file_name,
-        use_new_parser=True,
-        collected_errors=collected_errors,
+    """,
+        expected_line=4,
+        expected_column=47,
+        expected_end_column=64,
+        other_files={
+            "template.txt": "MY_KEYWORD <MY_KEYWORD>",
+            "priors.txt": "MY_KEYWORD NORMAL 0 1",
+        },
     )
-
-    error: ErrorInfo = collected_errors[0]
-    assert error.filename == test_config_file_name
-    assert error.line == 4
-    assert error.column == 47
-    assert error.end_column == 64
 
 
 @pytest.mark.usefixtures("use_tmpdir")
 def test_info_missing_forward_model_job(tmp_path):
-    with open(test_config_file_name, "w", encoding="utf-8") as fh:
-        fh.write(
-            """
+    assert_error_from_config_with(
+        contents="""
 JOBNAME my_name%d
 NUM_REALIZATIONS 1
 FORWARD_MODEL ECLIPSE9001
-"""
-        )
-
-    with open("template.txt", mode="w", encoding="utf-8") as fh:
-        fh.writelines("MY_KEYWORD <MY_KEYWORD>")
-
-    with open("priors.txt", mode="w", encoding="utf-8") as fh:
-        fh.writelines("MY_KEYWORD NORMAL 0 1")
-
-    collected_errors = []
-    ErtConfig.from_file(
-        test_config_file_name,
-        use_new_parser=True,
-        collected_errors=collected_errors,
+""",
+        expected_line=4,
+        expected_column=15,
+        expected_end_column=26,
+        other_files={
+            "template.txt": "MY_KEYWORD <MY_KEYWORD>",
+            "priors.txt": "MY_KEYWORD NORMAL 0 1",
+        },
     )
-
-    error: ErrorInfo = collected_errors[0]
-    assert error.filename == test_config_file_name
-    assert error.line == 4
-    assert error.column == 15
-    assert error.end_column == 26
 
 
 @pytest.mark.usefixtures("use_tmpdir")
-def test_info_positional_forward_model_args_gives_config_validation_error():
-    test_config_contents = dedent(
-        """
+def test_info_positional_forward_model_args_gives_error():
+    assert_error_from_config_with(
+        contents="""
 NUM_REALIZATIONS  1
 FORWARD_MODEL RMS <IENS>
-        """
-    )
-    with open(test_config_file_name, "w", encoding="utf-8") as fh:
-        fh.write(test_config_contents)
-
-    collected_errors = []
-    ErtConfig.from_file(
-        test_config_file_name, use_new_parser=True, collected_errors=collected_errors
+""",
+        expected_line=3,
+        expected_column=15,
+        expected_end_column=25,
     )
 
-    error: ErrorInfo = collected_errors[0]
-    assert error.filename == test_config_file_name
-    assert error.column == 15
-    assert error.end_column == 25
+
+@pytest.mark.usefixtures("use_tmpdir")
+def test_info_missing_simulation_job_gives_error():
+    assert_error_from_config_with(
+        contents="""
+NUM_REALIZATIONS  1
+SIMULATION_JOB this-is-not-the-job-you-are-looking-for hello
+        """,
+        expected_line=3,
+        expected_column=16,
+        expected_end_column=55,
+    )
+
+
+@pytest.mark.usefixtures("use_tmpdir")
+def test_info_unknown_hooked_job_gives_error():
+    assert_error_from_config_with(
+        contents="""
+NUM_REALIZATIONS  1
+HOOK_WORKFLOW NO_SUCH_JOB PRE_SIMULATION
+""",
+        expected_line=3,
+        expected_column=15,
+        expected_end_column=26,
+    )
+
+
+@pytest.mark.usefixtures("use_tmpdir")
+def test_that_non_exising_job_directory_gives_error():
+    assert_error_from_config_with(
+        contents="""
+NUM_REALIZATIONS  1
+DEFINE <STORAGE> storage/<CONFIG_FILE_BASE>-<DATE>
+RUNPATH <STORAGE>/runpath/realization-<IENS>/iter-<ITER>
+ENSPATH <STORAGE>/ensemble
+INSTALL_JOB_DIRECTORY does_not_exist
+""",
+        expected_line=6,
+        expected_column=23,
+        expected_end_column=37,
+    )
