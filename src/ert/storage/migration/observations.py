cfa622535202e19692d18b9ecd895b9696a7f71f
Yngve S. Kristiansen <yngve.s.kristiansen@webstep.no>
2024-03-22 08:56:44 +0100

    Combine observations & ds squash

--- src/ert/storage/migration/observations.py
+++ src/ert/storage/migration/observations.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os.path
 from typing import TYPE_CHECKING
 
 from ert.storage.local_storage import local_storage_get_ert_config
@@ -17,4 +18,9 @@ def migrate(path: Path) -> None:
             output_path = experiment / "observations"
             output_path.mkdir(parents=True, exist_ok=True)
             for name, dataset in observations.items():
+                obs_names = set(dataset["obs_name"].data)
+                for obs_name in obs_names:
+                    if os.path.exists(output_path / obs_name):
+                        os.remove(output_path / obs_name)
+
                 dataset.to_netcdf(output_path / f"{name}", engine="scipy")
