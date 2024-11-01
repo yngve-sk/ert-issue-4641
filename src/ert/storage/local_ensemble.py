yngve-sk/ert

Sync response config keys w/ actual response keys

src/ert/storage/local_ensemble.py
@@ -877,6 +877,9 @@ class LocalEnsemble(BaseMode):
             output_path / f"{response_type}.parquet", data
         )
 
+        response_keys = data["response_key"].unique().to_list()
+        self.experiment.update_response_keys(response_type, response_keys)
+
     def calculate_std_dev_for_parameter(self, parameter_group: str) -> xr.Dataset:
         if parameter_group not in self.experiment.parameter_configuration:
             raise ValueError(f"{parameter_group} is not registered to the experiment.")

yngve-sk/ert-issue-4641

src/ert/storage/local_ensemble.py
@@ -877,6 +877,9 @@ class LocalEnsemble(BaseMode):
             output_path / f"{response_type}.parquet", data
         )
 
+        response_keys = data["response_key"].unique().to_list()
+        self.experiment._update_response_keys(response_type, response_keys)
+
     def calculate_std_dev_for_parameter(self, parameter_group: str) -> xr.Dataset:
         if parameter_group not in self.experiment.parameter_configuration:
             raise ValueError(f"{parameter_group} is not registered to the experiment.")